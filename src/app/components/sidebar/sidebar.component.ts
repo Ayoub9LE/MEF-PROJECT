80ff531ff3dce2b1688eb751cee65ab17e7c75ae
Ayoub9LE <ayoublemniai9@gmail.com>
2023-07-08 09:49:20 +0000

    login and registration page

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-
+import { ElementRef, Renderer2, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  @ViewChild('body', { static: true }) bodyRef!: ElementRef;
+  @ViewChild('sidebar', { static: true }) sidebarRef!: ElementRef;
+  @ViewChild('toggle', { static: true }) toggleRef!: ElementRef;
+  @ViewChild('searchBtn', { static: true }) searchBtnRef!: ElementRef;
+  @ViewChild('modeSwitch', { static: true }) modeSwitchRef!: ElementRef;
+  @ViewChild('modeText', { static: true }) modeTextRef!: ElementRef;
 
+  constructor(private renderer: Renderer2) {}
+
+  toggleSidebar() {
+    this.renderer.addClass(this.sidebarRef.nativeElement, 'close');
+  }
+
+  openSidebar() {
+    this.renderer.removeClass(this.sidebarRef.nativeElement, 'close');
+  }
+
+  toggleMode() {
+    this.renderer.addClass(this.bodyRef.nativeElement, 'dark');
+
+    if (this.bodyRef.nativeElement.classList.contains('dark')) {
+      this.modeTextRef.nativeElement.innerText = 'Light mode';
+    } else {
+      this.modeTextRef.nativeElement.innerText = 'Dark mode';
+    }
+  }
 }
