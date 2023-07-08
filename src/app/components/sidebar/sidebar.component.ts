20505f41454f50ff3f695bdfa88fa65e67dda2ab
Ayoub9LE <ayoublemniai9@gmail.com>
2023-07-08 11:46:39 +0000

    siebar and navbar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -6,30 +6,10 @@ import { ElementRef, Renderer2, ViewChild } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
-  @ViewChild('body', { static: true }) bodyRef!: ElementRef;
-  @ViewChild('sidebar', { static: true }) sidebarRef!: ElementRef;
-  @ViewChild('toggle', { static: true }) toggleRef!: ElementRef;
-  @ViewChild('searchBtn', { static: true }) searchBtnRef!: ElementRef;
-  @ViewChild('modeSwitch', { static: true }) modeSwitchRef!: ElementRef;
-  @ViewChild('modeText', { static: true }) modeTextRef!: ElementRef;
-
-  constructor(private renderer: Renderer2) {}
-
-  toggleSidebar() {
-    this.renderer.addClass(this.sidebarRef.nativeElement, 'close');
-  }
-
-  openSidebar() {
-    this.renderer.removeClass(this.sidebarRef.nativeElement, 'close');
-  }
-
-  toggleMode() {
-    this.renderer.addClass(this.bodyRef.nativeElement, 'dark');
-
-    if (this.bodyRef.nativeElement.classList.contains('dark')) {
-      this.modeTextRef.nativeElement.innerText = 'Light mode';
-    } else {
-      this.modeTextRef.nativeElement.innerText = 'Dark mode';
-    }
-  }
+  //Sidebar toggle show hide function
+status = false;
+addToggle()
+{
+  this.status = !this.status;       
+}
 }
