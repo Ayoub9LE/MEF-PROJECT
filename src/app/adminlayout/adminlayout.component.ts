5eb1a9b5fc5ec5582b4d4ad9b9a613c5629c083f
Ayoub9LE <ayoublemniai9@gmail.com>
2023-07-18 23:20:45 +0000

    adding the statistics page

--- src/app/adminlayout/adminlayout.component.ts
+++ src/app/adminlayout/adminlayout.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit , ElementRef} from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-adminlayout',
   templateUrl: './adminlayout.component.html',
   styleUrls: ['./adminlayout.component.css']
 })
-export class AdminlayoutComponent {
+export class AdminlayoutComponent implements OnInit{
+  barChart: any;
+  polarChart: any;
 
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit(){
+    this.chartBar();
+    this.chartPolarArea();
+  }
+
+  chartBar(){
+    let htmlRef = this.elementRef.nativeElement.querySelector(`#barre`);
+    this.barChart = new Chart(htmlRef, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+
+  chartPolarArea(){
+    let htmlRef = this.elementRef.nativeElement.querySelector(`#circle`);
+    this.polarChart = new Chart(htmlRef, {
+      type: 'polarArea',
+      data: {
+        labels: ['Red', 'Green', 'Yellow', 'Grey', 'Blue'],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [11, 16, 7, 3, 14],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(75, 192, 192)',
+            'rgb(255, 205, 86)',
+            'rgb(201, 203, 207)',
+            'rgb(54, 162, 235)'
+          ]
+        }]
+      },
+      options: {}
+    });
+  }
 }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./test.component.css']
 })
 export class TestComponent {
-  
+  public isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
